821bd98a636407851a2ad7b11ca8bc05e21923bd
Brian Gitego <gitegob7@gmail.com>
2019-10-29 13:25:07 +0200

    feature(Citizen profile): Add profile page
    - create a user profile page
    - add script
    
    [ Finishes #169419362 ]

---
+++ UI/scripts/script.js
@@ -0,0 +1,12 @@
+const makeActive = (parameter) => {
+  parameter.forEach((el) => {
+    el.addEventListener('click', () => {
+      parameter.forEach((element) => {
+        element.classList.remove('active');
+      });
+      el.classList.add('active');
+    });
+  });
+};
+makeActive(document.querySelectorAll('.status-link-wrapper'));
+makeActive(document.querySelectorAll('.tab-link-wrapper'));
