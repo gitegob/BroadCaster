88d6212dfd8c4b948fd2c5f4119d6271945f166b
Brian Gitego <gitegob7@gmail.com>
2019-10-30 23:12:23 +0200

    feature(view record): Viewing a record
    - add view record page
    - add more styles
    - upgrade some of the other pages styles
    
    [ Finishes #169467683 ]

--- UI/scripts/script.js
+++ UI/scripts/script.js
@@ -1,13 +1,31 @@
-const makeActive = (parameter) => {
-  parameter.forEach((el) => {
-    el.addEventListener('click', () => {
-      parameter.forEach((elmt) => {
-        elmt.classList.remove('active');
+const makeActive = (elements) => {
+  elements.forEach((element) => {
+    element.addEventListener('click', () => {
+      elements.forEach((element) => {
+        element.classList.remove('active');
       });
-      el.classList.add('active');
+      element.classList.add('active');
     });
   });
 };
-makeActive(document.querySelectorAll('.status-link-wrapper'));
+makeActive(document.querySelectorAll('.admin-page .status-link-wrapper'));
 makeActive(document.querySelectorAll('.tab-link-wrapper'));
 makeActive(document.querySelectorAll('div.page'));
+
+const setStatusColor = (param) => {
+  if (param.value === 'resolved') {
+    param.style.color = 'green';
+  }
+  if (param.value === 'unresolved') {
+    param.style.color = 'orange';
+  }
+  if (param.value === 'rejected') {
+    param.style.color = 'red';
+  }
+};
+document.querySelectorAll('select').forEach((element) => {
+  setStatusColor(element);
+  element.addEventListener('change', () => {
+    setStatusColor(element);
+  });
+});
