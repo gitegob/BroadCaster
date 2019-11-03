1d691155df1d265ea784948ba11a97a5cc871e49
Brian Gitego <gitegob7@gmail.com>
2019-11-03 19:00:53 +0200

    chore(API setup): API setup
    - download dependencies
    - setup the folder structure
    - add base route
    - write tests for base route
    
    [ Finishes #169522061 ]

---
+++ server/app.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
+import morgan from 'morgan';
+import { config } from 'dotenv';
+import Helpers from './v1/helpers/helpers';
+
+config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(morgan('dev'));
+app.use(json());
+app.use(
+  urlencoded({
+    extended: false,
+  }),
+);
+app.get('/', (req, res) => {
+  Helpers.sendSuccess(res, 200, 'Welcome to BroadCaster');
+});
+app.use('/*', (_req, res) => {
+  Helpers.sendError(res, 404, 'Not Found');
+});
+
+app.use((error, _req, res, _next) => {
+  if (error.status === 400) {
+    Helpers.sendError(
+      res,
+      error.status,
+      'Syntax error, Please double check your input',
+    );
+  } else {
+    Helpers.sendError(res, error.status || 500, 'SERVER DOWN!');
+  }
+});
+
+app.listen(port, () => {
+  process.stdout.write(`Connected on ${port}`);
+});
+
+export default app;
